7c8735988eefe69be4ae9f62009341819f08e78e
amir-shirani <59798686+amir-shirani@users.noreply.github.com>
2020-01-18 12:02:40 +0330

    start

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>cms</title>
+</head>
+<body>
+    <div class="head container-fluid">
+        <div class="container">
+            <div class="row">
+                <h1>amir shirani</h1>
+                <ul>
+                    <li>خانه</li>
+                    <li>درباره ما</li>
+                    <li>ارتباط با ما</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container bg-light pt-5">
+        <div class="row">
+            <div class="col-8">
+                <img class="imgban" src="assets/image/banner-end.jpg" alt="">
+            </div>
+            <div class="col-3">
+                <div class="side">
+                    <h2 class="tittle">آموزش</h2>
+                    <p class="sub-tittle">
+                        با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات
+                    </p>
+                    <p class="sub-tittle">
+                        باباباباباب به خدا پنجشمبه اول برین شما یا علی چندی اینجا شلوغ باباب اهه چه وع1ضیه                     </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <h2 class="tittle">دا پنجشمبه اول برین شما دهن دست من دلقتلثذ باباب اهه چه وع1ضیه                 </h2>
+                <p class="sub-tittle mt-5">
+                    با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات توسعه  ور ودمنبدبذم  جرا شبا هیچا این راس میگه ایولا حرف حق بود حیات عجی حوصله ام سر رفت چندی بترتابیتایتبنلایلن هعلذنتلبنتلذ نلذنتنی با امیر رلیخ دت بغ د ئست کعار یهئ دیدم که بتهدتنمدخم
+                    با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات توسعه  ور ودمنبدبذم  جرا شبا هیچا این راس میگه ایولا حرف حق بود حیات عجی حوص
+                </p>
+                <button class="btn-my">more</button>
+            </div>
+            <div class="col-3">
+                <div class="side">
+                    <h2 class="tittle">video</h2>
+                    <p class="sub-tittle">
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 mt-5">
+                <img class="imgban" src="assets/image/banner-end.jpg" alt="">
+            </div>
+            <div class="col-3">
+                <div class="side">
+                    <h2 class="tittle">آموزش</h2>
+                    <p class="sub-tittle">
+                        با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات
+                    </p>
+                    <p class="sub-tittle">
+                        باباباباباب به خدا پنجشمبه اول برین شما یا علی چندی اینجا شلوغ باباب اهه چه وع1ضیه                     </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8">
+                <h2 class="tittle">دا پنجشمبه اول برین شما دهن دست من دلقتلثذ باباب اهه چه وع1ضیه                 </h2>
+                <p class="sub-tittle mt-5">
+                    با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات توسعه  ور ودمنبدبذم  جرا شبا هیچا این راس میگه ایولا حرف حق بود حیات عجی حوصله ام سر رفت چندی بترتابیتایتبنلایلن هعلذنتلبنتلذ نلذنتنی با امیر رلیخ دت بغ د ئست کعار یهئ دیدم که بتهدتنمدخم
+                    با سلام و عرض ادب این سایت برای اموزش و درس دادن به دانش اموز میباشد درصورتی که میخواهید از خدمات این سایت استفاده منید در صفحه هات توسعه  ور ودمنبدبذم  جرا شبا هیچا این راس میگه ایولا حرف حق بود حیات عجی حوص
+                </p>
+                <button class="btn-my">more</button>
+            </div>
+    </div>
+</body>
+</html>
